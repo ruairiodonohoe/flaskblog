f64c7a364766919fe5e0f8d0e13661846b65f9e9
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-06-12 21:49:48 +0100

    refactored file structure

--- src/flaskblog/__init__.py
+++ src/flaskblog/__init__.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flaskblog.forms import RegistrationForm, LoginForm
-from datetime import datetime, timezone
 
 
 app = Flask(__name__)
@@ -10,76 +8,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image = db.Column(db.String(20), nullable=False, default="default.jpeg")
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=True, nullable=False)
-    date_posted = db.Column(
-        db.DateTime(timezone=True),
-        nullable=False,
-        default=lambda: datetime.now(timezone.utc),
-    )
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-
-posts = [
-    {
-        "author": "Ruairi O'Donohoe",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "April 21, 2018",
-    },
-    {
-        "author": "John Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "April 21, 2018",
-    },
-]
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html", posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
-    return render_template("register.html", title="Register", form=form)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == "admin@blog.com" and form.password.data == "password":
-            flash("You have been logged in!", "success")
-            return redirect(url_for("home"))
-        else:
-            flash("Login Unsuccessful. Please check username and password!", "danger")
-    return render_template("login.html", title="Register", form=form)
+from flaskblog import routes
