aee7d6cc1151adbde1bafa63a01ca9ee5c914b27
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-06-12 20:27:02 +0100

    Added Sign Up and Log In

--- src/flaskblog/forms.py
+++ src/flaskblog/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistractionForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
