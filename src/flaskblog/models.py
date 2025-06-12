28ab9eebc0f637bdaeb50aef01e263e96f72e58c
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-06-13 00:01:02 +0100

    Update profile info

--- src/flaskblog/models.py
+++ src/flaskblog/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image = db.Column(db.String(20), nullable=False, default="default.jpeg")
+    image = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship("Post", backref="author", lazy=True)
 
